ad37e0f9294b9294bc7bdf8a496014552163df7c
Piotr Jasiński <pj406169@students.mimuw.edu.pl>
2020-02-12 19:29:23 +0100

    shamans

--- shamans/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/piotr/Pulpit/Studia/PW/shamans/src
-# Build directory: /home/piotr/Pulpit/Studia/PW/shamans/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- shamans/src/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/piotr/Pulpit/Studia/PW/shamans/src/tests
-# Build directory: /home/piotr/Pulpit/Studia/PW/shamans/src/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
